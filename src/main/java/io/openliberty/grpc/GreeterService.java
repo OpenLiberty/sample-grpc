dddf25ab1dc882986df67e522a3e0cb3d4880b9e
Bill Lucy <wtlucy@gmail.com>
2020-10-19 15:41:47 -0400

    add additional RPC examples

--- src/main/java/io/openliberty/grpc/GreeterService.java
+++ src/main/java/io/openliberty/grpc/GreeterService.java
@@ -1,8 +1,9 @@
 package io.openliberty.grpc;
 
+import java.util.logging.Logger;
+
 import javax.annotation.security.RolesAllowed;
 
-import io.grpc.InternalStatus;
 import io.grpc.stub.StreamObserver;
 import io.openliberty.grpc.interceptors.GreeterServiceInterceptor;
 import io.openliberty.sample.GreetingRequest;
@@ -14,9 +15,14 @@ import io.openliberty.sample.SampleServiceGrpc;
  */
 public class GreeterService extends SampleServiceGrpc.SampleServiceImplBase {
 
+    private static final Logger logger = Logger.getLogger(GreeterService.class.getName());
+
     // required public zero-arg constructor
     public GreeterService(){}
 
+    /**
+     * Unary RPC example
+     */
     @Override
     @RolesAllowed("Role1")
     public void getGreeting(GreetingRequest req, StreamObserver<GreetingResponse> resp) {
@@ -46,4 +52,91 @@ public class GreeterService extends SampleServiceGrpc.SampleServiceImplBase {
         resp.onNext(responseString);
         resp.onCompleted();
     }
+
+    /**
+     * Server Streaming RPC example
+     */
+    @Override
+    @RolesAllowed({"Role1", "Role2"})
+    public void getServerStreamingGreeting(GreetingRequest req, StreamObserver<GreetingResponse> resp) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Hello ");
+        builder.append(req.getName());
+
+        // stream the first response message containing the given name
+        GreetingResponse responseString = GreetingResponse.newBuilder().setMessage(builder.toString()).build();
+        resp.onNext(responseString);
+
+        // stream a second response message
+        responseString = GreetingResponse.newBuilder().setMessage("Have a nice day").build();
+        resp.onNext(responseString);
+
+        resp.onCompleted();
+    }
+
+    /**
+     * Client Streaming RPC example
+     */
+    @Override
+    @RolesAllowed("Role2")
+    public StreamObserver<GreetingRequest> getClientStreamingGreeting(StreamObserver<GreetingResponse> resp) {
+
+        StreamObserver<GreetingRequest> clientObserver = new StreamObserver<GreetingRequest>() {
+
+            StringBuilder builder = new StringBuilder();
+
+            @Override
+            public void onNext(GreetingRequest req) {
+                // save every name that the client streams over
+                builder.append(req.getName());
+                builder.append(" ");
+            }
+
+            @Override
+            public void onCompleted() {
+                // respond with every name that the client has streamed
+                GreetingResponse response = GreetingResponse.newBuilder().setMessage("Hello " + builder.toString()).build();
+                resp.onNext(response);
+                resp.onCompleted();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                logger.info("getClientStreamingGreeting error: " + t);
+            }
+        };
+
+        return clientObserver;
+    }
+
+    /**
+     * Bidirectional Streaming RPC example
+     */
+    @Override
+    @RolesAllowed({"Role1", "Role2"})
+    public StreamObserver<GreetingRequest> getBidirectionalGreeting(StreamObserver<GreetingResponse> resp) {
+
+            StreamObserver<GreetingRequest> bidiObserver = new StreamObserver<GreetingRequest>() {
+
+                @Override
+                public void onNext(GreetingRequest req) {
+                    // send a response every time the client streams a name
+                    GreetingResponse response = GreetingResponse.newBuilder().setMessage("Hello " + req.getName()).build();
+                    resp.onNext(response);
+                }
+
+                @Override
+                public void onCompleted() {
+                    resp.onCompleted();
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    logger.info("getBidirectionalGreeting error: " + t);
+                }
+            };
+
+            return bidiObserver;
+    }
+
 }
